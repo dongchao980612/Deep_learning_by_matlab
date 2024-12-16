0c9f8cfb977173bc424570d43382b467136d3ff2
dongchao <dongchao980612@163.com>
2024-12-16 12:41:30 +0800

    modefied test.m

--- 第1章 基于直方图优化的图像去雾技术/test.m
+++ 第1章 基于直方图优化的图像去雾技术/test.m
@@ -7,13 +7,13 @@ I = imread('MonaLisa.png');
 res_RemoveFogByGlobalHisteq = RemoveFogByGlobalHisteq(I,1);
 
 % 局部直方图处理
-res_RemoveFogByLocalHisteq= RemoveFogByLocalHisteq(I,0);
+%res_RemoveFogByLocalHisteq= RemoveFogByLocalHisteq(I,0);
 
 % Retinex增强处理
-res_RemoveFogByRetinex= RemoveFogByRetinex(I,0);
+%res_RemoveFogByRetinex= RemoveFogByRetinex(I,0);
 
-figure; 
-subplot(2, 2, 1); imshow(I); title('原图');
-subplot(2, 2, 2); imshow(res_RemoveFogByGlobalHisteq); title('全局直方图处理');
-subplot(2, 2, 3); imshow(res_RemoveFogByLocalHisteq); title('局部直方图处理')
-subplot(2, 2, 4); imshow(res_RemoveFogByRetinex); title('Retinex增强处理')
+%figure; 
+%subplot(2, 2, 1); imshow(I); title('原图');
+%subplot(2, 2, 2); imshow(res_RemoveFogByGlobalHisteq); title('全局直方图处理');
+%subplot(2, 2, 3); imshow(res_RemoveFogByLocalHisteq); title('局部直方图处理')
+%subplot(2, 2, 4); imshow(res_RemoveFogByRetinex); title('Retinex增强处理')
